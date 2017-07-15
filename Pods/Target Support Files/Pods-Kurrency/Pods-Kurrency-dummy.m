da97e434e8696decc4e14fb27f5c71a827ebba92
Ard Stavileci <ardstavileci@gmail.com>
2017-07-15 13:11:50 +0200

    First commit

---
+++ Pods/Target Support Files/Pods-Kurrency/Pods-Kurrency-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Kurrency : NSObject
+@end
+@implementation PodsDummy_Pods_Kurrency
+@end
---
+++ Pods/Target Support Files/Pods-KurrencyTests/Pods-KurrencyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KurrencyTests : NSObject
+@end
+@implementation PodsDummy_Pods_KurrencyTests
+@end
